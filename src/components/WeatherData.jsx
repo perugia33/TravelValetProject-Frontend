14f8e5f690cfe86fafce93c0927bd2b870358155
exitexit <eunicegore@gmail.com>
2024-08-08 19:21:59 -0700

    connecting weather display feature to backend

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,17 +1,19 @@
    import styles from './Weather.module.css'    
 
-   function WeatherData({weather}) {
+    function WeatherData({weather}) {
+        const weatherData = weather || [];
         return (
             <div className={styles.weatherContainer}>
-               {weather.map((day, index) => (
+                {weatherData.map((day, index) => (
                 <div key={index} className={styles.weatherData}>
                     <div className={styles.day}>{day.day}</div>
                     <div className={styles.temp}>
-                         <div className={styles.condition}>{day.condition}</div>
-                        <span className={styles.minTemp}>{day.minTemp}°F</span>
-                        <span className={styles.maxTemp}>{day.maxTemp}°F</span>
+                            {/* <div className={styles.condition}>{day.condition}</div> */}
+                            <img src={`http://openweathermap.org/img/wn/${day.condition}.png`} alt="Weather icon" />
+                        <span className={styles.minTemp}>{day.minTemp.toFixed(1)}°F</span>
+                        <span className={styles.maxTemp}>{day.maxTemp.toFixed(1)}°F</span>
                     </div>
-                   
+                    
 
                     </div>
             ))}
