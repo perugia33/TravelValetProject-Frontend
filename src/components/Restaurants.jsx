a8dab4b1317e63caf0dc7658e41d596946e94616
Ivana <ivana.a.maldonado@gmail.com>
2024-08-09 04:26:29 -0400

    Integrates restaurant feature with backend

--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -1,4 +1,5 @@
 import  styles from './Restaurants.module.css'
+
 function Restaurants({ recommendations }) {
     return (
         <div className={styles.container}>
@@ -6,21 +7,23 @@ function Restaurants({ recommendations }) {
             {recommendations.length === 0 ? (
                 <p>No recommendations available.</p>
             ) : (
-                recommendations.map((rec) =>    
+                recommendations.map((rec) => (  
                 <div  key={rec.id} className="restaurant">
-                    <img src="{rec.image}" alt="{rec.name}"/>
+                {/*Commented code is to help with styling <div key={rec.id} className={styles.restaurant}>  */}
+                    <img src={rec.image_url} alt={rec.name}/>
+                    {/* <img src={rec.image_url} alt={rec.name} className={styles.image} /> */}
                     <div className= "restaurant-details">
+                    {/* <div className={styles.restaurantDetails}> */}
                         <h3>{rec.name}</h3>
                         <p>{rec.address}</p>
                         <p>Rating: {rec.rating} ({rec.review_count} reviews)</p>
-                        <p><a href="{rec.website}">Visit Website</a></p>
+                        <p><a href={rec.website} target="_blank" rel="noopener noreferrer">Visit Website</a></p>
                     </div>
-            </div>
-
-                )
+                </div>
+                ))
             )}
         </div>
-    )
+    );
 }
 
 export default Restaurants
