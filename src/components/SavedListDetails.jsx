8c2d20efe1debdb1c73a153f419c8e2e4c64cd20
Ivana <ivana.a.maldonado@gmail.com>
2024-08-14 00:36:35 -0400

    refactor client for api calls in packing list

--- src/components/SavedListDetails.jsx
+++ src/components/SavedListDetails.jsx
@@ -1,24 +1,26 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import PackList from "./PackList";
 import PackingListForm from "./PackingListForm";
 import ProgressTracker from "./ProgressTracker";
 import PackLogo from "../components/PackLogo";
 import NavBar from "../components/NavBar";
-import useApi from "../services/api";
+// import useApi from "../services/api";
+import { AuthContext } from "../contexts/AuthContext.jsx";
 
 function SavedListDetails() {
   const { listId } = useParams(); // Use URL params to get the listId
   const [list, setList] = useState(null);
   const [items, setItems] = useState([]);
-  const api = useApi();
+  const clientApi = useContext(AuthContext);
+  // const api = useApi();
 
   // Get one list by id:
   useEffect(() => {
     const fetchListDetails = async () => {
       try {
         if (listId) {
-          const response = await api.get(`/${listId}`);
+          const response = await clientApi.get(`packing-list/${listId}`);
           setList(response.data);
           setItems(response.data.items || []);
         }
@@ -28,12 +30,12 @@ function SavedListDetails() {
     };
 
     fetchListDetails();
-  }, [listId, api]);
+  }, [listId, clientApi]);
 
   // Remove item from a list by id:
   const handleRemoveItem = async (itemId) => {
     try {
-      await api.delete(`/${listId}/items/${itemId}`);
+      await clientApi.delete(`packing-list/${listId}/items/${itemId}`);
       // Update the state to remove the item
       setItems(items.filter((item) => item.id !== itemId));
     } catch (error) {
@@ -44,7 +46,7 @@ function SavedListDetails() {
   // Toggle item's 'packed' state:
   const handleToggleItem = async (itemId) => {
     try {
-      const response = await api.patch(`/${listId}/items/${itemId}/toggle`);
+      const response = await clientApi.patch(`packing-list/${listId}/items/${itemId}/toggle`);
       const updatedItem = response.data.item;
       setItems(
         items.map((item) =>
@@ -59,7 +61,7 @@ function SavedListDetails() {
   // Add item to a saved list:
   const handleAddItem = async (newItem) => {
     try {
-      const response = await api.post(`/${listId}/items`, newItem);
+      const response = await clientApi.post(`packing-list/${listId}/items`, newItem);
       const addedItem = response.data.item;
       // Update state with the newly added item:
       setItems([...items, addedItem]);
@@ -77,7 +79,7 @@ function SavedListDetails() {
     if (newListName !== null) {
       try {
         // Send the updated list name and items to the backend
-        const response = await api.put(`/${listId}`, {
+        const response = await clientApi.put(`packing-list/${listId}`, {
           listName: newListName || list.listName, // Use the current list name if none is provided
           items: items,
         });
@@ -96,7 +98,7 @@ function SavedListDetails() {
   // Update item's description and quantity:
   const handleUpdateItem = async (itemId, updatedData) => {
     try {
-      const response = await api.put(`/${listId}/items/${itemId}`, updatedData);
+      const response = await clientApi.put(`packing-list/${listId}/items/${itemId}`, updatedData);
       setItems(
         items.map((item) =>
           item.id === itemId ? { ...item, ...updatedData } : item
@@ -112,7 +114,7 @@ function SavedListDetails() {
   const handleClearAllItemsSaved = async () => {
     try {
       // Call API to remove all items from the backed
-      await api.delete(`/${listId}/items`);
+      await clientApi.delete(`packing-list/${listId}/items`);
 
       // Clear items from local state
       setItems([]);
