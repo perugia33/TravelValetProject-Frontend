8ee8e53622676ccfcb7432a3804a0433e8f6b77a
exitexit <eunicegore@gmail.com>
2024-08-14 00:23:12 -0700

    refatoring clientApi

--- src/services/clientApi.js
+++ src/services/clientApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const clientApi = axios.create({
-  baseURL: 'https://back-end-travel-valet.onrender.com/', 
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/',
+  // baseURL: 'https://back-end-travel-valet.onrender.com/', 
 });
 export default clientApi;
