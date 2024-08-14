df361b617f6126629a25c2c91d0877097c43a8d3
exitexit <eunicegore@gmail.com>
2024-08-14 11:27:45 -0700

    success functioning on local

--- src/services/clientApi.js
+++ src/services/clientApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const clientApi = axios.create({
-  // baseURL: 'http://localhost:5000/',
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/',
+  baseURL: 'http://127.0.0.1:5000/',
+  // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/',
   // baseURL: 'https://back-end-travel-valet.onrender.com/', 
 });
 export default clientApi;
